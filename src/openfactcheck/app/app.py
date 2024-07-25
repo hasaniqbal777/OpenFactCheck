95bc6ed584d5a0b9a2e1bca61f739f42ea3da30f
Hasan Iqbal <34169605+hasaniqbal777@users.noreply.github.com>
2024-07-25 15:22:05 +0400

    Set theme to light mode permenantly

--- src/openfactcheck/app/app.py
+++ src/openfactcheck/app/app.py
@@ -12,14 +12,25 @@ def greet(name):
     """
     return f"Hello, {name}! Welcome to Gradio."
 
-# Define the Gradio interface
-demo = gr.Interface(
+js_func = """
+function refresh() {
+    const url = new URL(window.location);
+
+    if (url.searchParams.get('__theme') !== 'light') {
+        url.searchParams.set('__theme', 'light');
+        window.location.href = url.href;
+    }
+}
+"""
+
+with gr.Blocks(js=js_func) as demo:
+    gr.Interface(
     fn=greet,  # Function to call
     inputs=gr.Textbox(placeholder="Enter your name here...", label="Name"),  # Input component
     outputs="text",  # Output component type
     title="Hello World App",  # Title of the app
     description="A simple Gradio app that greets you."  # Description of the app
-)
+    )
 
 # Run the interface
 if __name__ == "__main__":
